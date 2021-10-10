76d38dbfa7bba2e1dbeb332e87ba9196adfeeb21
Nenad <nk@orgatech.org>
2021-10-10 23:10:32 +0200

    xdgj

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-footer',
@@ -9,7 +10,9 @@ import { Router } from '@angular/router';
 export class FooterComponent implements OnInit {
     test : Date = new Date();
 
-    constructor(private router: Router ) {}
+    constructor(private router: Router,  public translate: TranslateService  ) { 
+      translate.addLangs(['rs', 'nl']);  
+      translate.setDefaultLang('rs');  }
 
     ngOnInit() {
 
@@ -17,4 +20,9 @@ export class FooterComponent implements OnInit {
     getPath(){
       return this.router.url;
     }
+    switchLang(lang){
+      this.translate.use(lang);
+      console.log(this.translate.currentLang);
+  }
+
 }
