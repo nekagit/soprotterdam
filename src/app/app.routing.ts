e14e908ab52bddf5970206d18c7b9d980a7c523b
Nenad <nk@orgatech.org>
2021-11-06 12:19:28 +0100

    eh

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -10,16 +10,27 @@ import { KontaktComponent } from './kontakt/kontakt.component';
 import { SkolaComponent } from './skola/skola.component';
 import { VestComponent } from './vest/vest.component';
 import { UdzComponent } from './udz/udz.component';
+import { HomeSComponent } from './home-s/home-s.component';
+import { ProfileSComponent } from './profile-s/profile-s.component';
+import { KontaktSComponent } from './kontakt-s/kontakt-s.component';
+import { VestSComponent } from './vest-s/vest-s.component';
+import { UdzSComponent } from './udz-s/udz-s.component';
 
 const routes: Routes =[
-    { path: 'pocetna',             component: HomeComponent },
-    { path: 'galerija',     component: ProfileComponent },
-    { path: 'aktivnosti',          component: LandingComponent },
-    { path: 'kontakt',          component: KontaktComponent },
-    { path: 'skola',          component: SkolaComponent },
-    { path: 'vest',          component: VestComponent },
-    { path: 'udz',          component: UdzComponent },
-    { path: '', redirectTo: 'pocetna', pathMatch: 'full' }
+  { path: 'pocetna',             component: HomeComponent },
+  { path: 'galerija',     component: ProfileComponent },
+  { path: 'aktivnosti',          component: LandingComponent },
+  { path: 'kontakt',          component: KontaktComponent },
+  { path: 'skola',          component: SkolaComponent },
+  { path: 'vest',          component: VestComponent },
+  { path: 'udz',          component: UdzComponent },
+  { path: 'pocetnaS',             component: HomeSComponent },
+  { path: 'galerijaS',     component: ProfileSComponent },
+  { path: 'kontaktS',          component: KontaktSComponent },
+  { path: 'skola',          component: SkolaComponent },
+  { path: 'vestS',          component: VestSComponent },
+  { path: 'udzS',          component: UdzSComponent },
+    { path: '', redirectTo: 'prva', pathMatch: 'full' }
 ];
 
 @NgModule({
