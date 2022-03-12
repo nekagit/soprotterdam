08e2d7fa72d6efc985014c6090b9cc5e3d3ebe36
Nenad <nk@orgatech.org>
2022-03-12 19:22:13 +0100

    asdf

--- src/app/vest/vest.component.ts
+++ src/app/vest/vest.component.ts
@@ -1,25 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarService } from '../navbar.service';
+import vestiText from '../../assets/vesti.json';
 
 @Component({
   selector: 'app-vest',
   templateUrl: './vest.component.html',
-  styleUrls: ['./vest.component.css']
+  styleUrls: ['./vest.component.css'],
 })
+
 export class VestComponent implements OnInit {
-
-  constructor(public nav: NavbarService) { }
-
-  ngOnInit() {this.nav.showS();
   
-    document.getElementById("slk").onmouseover = function ()  {
-      document.getElementById("slk").classList.add('hoverIn');
-      document.getElementById("slk").classList.remove('hoverOut');
+  vestiJson = vestiText;  
+  vestiJsonIndex: string[] = ['0','3'];
+  vestiExtension: string[] = ['.jpeg', '.jpeg']
+  directoryPath: String = '../../assets/img/skola/' 
+  frontImg = document.getElementById("frontImg") as HTMLImageElement;
 
-  };
-  document.getElementById("slk").onmouseleave = function ()  {
-      document.getElementById("slk").classList.add('hoverOut');
-      document.getElementById("slk").classList.remove('hoverIn');
-  };}
+  currentVestiNumber: number = 0;
+  currentVestiString: string = this.currentVestiNumber + '';
+  currentVestiExtension = this.vestiExtension[this.currentVestiNumber]  
+  getImgPathEnding() {
+    return this.vestiJsonIndex[this.currentVestiNumber] + this.vestiExtension[this.currentVestiNumber]
+  }
+  imagePathEnding = this.getImgPathEnding()
 
+  getImagePath() {
+    return this.directoryPath + this.getImgPathEnding()
+  } 
+  getFrontText() {
+    return this.vestiJson[this.vestiJsonIndex[this.currentVestiNumber]];
+  }
+
+  frontText: string = this.getFrontText()
+  imagePath: string = this.getImagePath()
+  tmpImg: string = "";
+  
+  getNewVesti() {
+    this.frontText = this.getFrontText();
+    this.imagePath = this.getImagePath()
+    console.log(this.imagePath)
+    this.tmpImg = '<img id="frontImg" class="frontImage" [src]="' + this.imagePath +' />';
+    this.frontImg.replaceWith(this.tmpImg) 
+    
+  }
+
+  
+
+
+  constructor(public nav: NavbarService) {}
+
+  ngOnInit() {
+    this.nav.showS();
+
+    document.getElementById('slk').onmouseover = function () {
+      document.getElementById('slk').classList.add('hoverIn');
+      document.getElementById('slk').classList.remove('hoverOut');
+    };
+    document.getElementById('slk').onmouseleave = function () {
+      document.getElementById('slk').classList.add('hoverOut');
+      document.getElementById('slk').classList.remove('hoverIn');
+    };
+  }
+  
+  getNextVesti() {
+    console.log(this.currentVestiNumber)
+    if(this.currentVestiNumber < 2 && this.currentVestiNumber >= 0) {
+      
+    this.currentVestiNumber = this.currentVestiNumber +1;
+    this.getNewVesti()
+    }
+  }
+  getPreviousVesti() {
+    if(this.currentVestiNumber < 2 && this.currentVestiNumber >= 0) {
+      
+      this.currentVestiNumber = this.currentVestiNumber -1;
+      this.getNewVesti()
+      }
+  }
 }
